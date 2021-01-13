ab018b2c1a3c7ee65b5d3030fb91446cc832b3c1
Lucas Braz <lmonteirobraz@gmail.com>
2021-01-13 13:52:09 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/lucasmbraz/flutter_todo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lucasmbraz.flutter_todo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/todo_app.dart';
+
+void main() {
+  runApp(TodoApp());
+}

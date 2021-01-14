cccf3f7a72fe7f83ff7e9bce82310e82cd3b755b
Lucas Braz <lmonteirobraz@gmail.com>
2021-01-14 13:45:42 +0100

    Code clean up

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -5,40 +5,30 @@ import 'package:flutter_todo/home/todos_list_widget.dart';
 import 'package:flutter_todo/model/todo_list.dart';
 import 'package:flutter_todo/redux/actions.dart';
 import 'package:flutter_todo/redux/app_state.dart';
-import 'package:redux/redux.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoreConnector<AppState, _ViewModel>(
-      converter: (store) => _ViewModel.from(store),
-      builder: (context, vm) => Scaffold(
-        appBar: AppBar(
-          title: Text('TODO'),
-        ),
-        body: TodosListWidget(),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => _openCreateListDialog(context, vm.callback),
-          child: const Icon(Icons.add),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TODO'),
+      ),
+      body: TodosListWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _openAddTodoListDialog(context),
+        child: const Icon(Icons.add),
       ),
     );
   }
 
-  _openCreateListDialog(BuildContext context, OnAddTodoListCallback callback) {
+  _openAddTodoListDialog(BuildContext context) {
     showDialog(
       context: context,
-      builder: (context) => AddTodoListDialog(callback),
+      builder: (context) => StoreBuilder<AppState>(
+        builder: (context, store) => AddTodoListDialog(
+          (listName) => store.dispatch(AddTodoListAction(TodoList(name: listName))),
+        ),
+      ),
     );
   }
-}
-
-class _ViewModel {
-  final OnAddTodoListCallback callback;
-
-  _ViewModel(this.callback);
-
-  static _ViewModel from(Store<AppState> store) => _ViewModel(
-        (listName) => store.dispatch(AddTodoListAction(TodoList(name: listName))),
-      );
 }

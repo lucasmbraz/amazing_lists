d76a6b0b6a133353f1671d4e132194d1a3349619
Lucas Braz <lmonteirobraz@gmail.com>
2021-01-24 17:51:26 +0100

    Code clean up

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_todo/add_todo_list/add_todo_list_dialog.dart';
+import 'package:flutter_todo/home/todos_list_item_widget.dart';
 import 'package:flutter_todo/home/todos_list_widget.dart';
 import 'package:flutter_todo/model/todo_list.dart';
 import 'package:flutter_todo/redux/actions.dart';
@@ -13,28 +14,51 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StoreBuilder<AppState>(builder: (context, store) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('TODO'),
-        ),
-        body: TodosListWidget(
-          onTapCallback: (todoList) => store.dispatch(DeleteTodoListAction(todoList)),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => _openAddTodoListDialog(context, store),
-          child: const Icon(Icons.add),
-        ),
-      );
-    });
+    return StoreConnector<AppState, _ViewModel>(
+        converter: (store) => _ViewModel.from(store, uuid),
+        builder: (context, vm) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('TODO'),
+            ),
+            body: _body(vm),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => _openAddTodoListDialog(context, vm),
+              child: const Icon(Icons.add),
+            ),
+          );
+        });
   }
 
-  _openAddTodoListDialog(BuildContext context, Store<AppState> store) {
-    showDialog(
-      context: context,
-      builder: (context) => AddTodoListDialog(
-        (listName) => store.dispatch(AddTodoListAction(TodoList(id: uuid.v4(), name: listName))),
-      ),
+  Widget _body(_ViewModel vm) {
+    return TodosListWidget(
+      todoLists: vm.todoLists,
+      onTapCallback: vm.onTapTodoListCallback,
     );
   }
+
+  _openAddTodoListDialog(BuildContext context, _ViewModel vm) {
+    showDialog(
+      context: context,
+      builder: (context) => AddTodoListDialog(vm.onAddTodoListCallback),
+    );
+  }
+}
+
+class _ViewModel {
+  _ViewModel(
+    this.todoLists, {
+    @required this.onAddTodoListCallback,
+    @required this.onTapTodoListCallback,
+  });
+
+  final List<TodoList> todoLists;
+  final OnAddTodoListCallback onAddTodoListCallback;
+  final OnTapTodoListCallback onTapTodoListCallback;
+
+  static _ViewModel from(Store<AppState> store, Uuid uuid) => _ViewModel(
+        store.state.todoLists,
+        onAddTodoListCallback: (listName) => store.dispatch(AddTodoListAction(TodoList(id: uuid.v4(), name: listName))),
+        onTapTodoListCallback: (todoList) => store.dispatch(DeleteTodoListAction(todoList)),
+      );
 }
--- lib/home/todos_list_widget.dart
+++ lib/home/todos_list_widget.dart
@@ -1,39 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_todo/home/todos_list_item_widget.dart';
 import 'package:flutter_todo/model/todo_list.dart';
-import 'package:flutter_todo/redux/app_state.dart';
-import 'package:redux/redux.dart';
 
 class TodosListWidget extends StatelessWidget {
   const TodosListWidget({
     Key key,
+    @required this.todoLists,
     @required this.onTapCallback,
   }) : super(key: key);
 
+  final List<TodoList> todoLists;
   final OnTapTodoListCallback onTapCallback;
 
   @override
   Widget build(BuildContext context) {
-    return StoreConnector<AppState, _TodosListViewModel>(
-        converter: (store) => _TodosListViewModel.from(store),
-        builder: (context, vm) {
-          return ListView.separated(
-            itemCount: vm.todoLists.length,
-            separatorBuilder: (context, index) => const Divider(),
-            itemBuilder: (context, index) => TodosListItemWidget(
-              todoList: vm.todoLists[index],
-              onTapCallback: onTapCallback,
-            ),
-          );
-        });
+    return ListView.separated(
+      itemCount: todoLists.length,
+      separatorBuilder: (context, index) => const Divider(),
+      itemBuilder: (context, index) => TodosListItemWidget(
+        todoList: todoLists[index],
+        onTapCallback: onTapCallback,
+      ),
+    );
   }
-}
-
-class _TodosListViewModel {
-  final List<TodoList> todoLists;
-
-  _TodosListViewModel(this.todoLists);
-
-  static _TodosListViewModel from(Store<AppState> store) => _TodosListViewModel(store.state.todoLists);
 }

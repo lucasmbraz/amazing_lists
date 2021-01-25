0aba44f64e9e7d5b757333e580b8c6f16da6870c
Lucas Braz <lmonteirobraz@gmail.com>
2021-01-25 17:47:32 +0100

    Change AppState.init to have at least one todo

--- lib/redux/app_state.dart
+++ lib/redux/app_state.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_todo/model/todo.dart';
 import 'package:flutter_todo/model/todo_list.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -13,7 +14,7 @@ abstract class AppState implements _$AppState {
 
   factory AppState.init() => AppState(
         todoLists: [
-          TodoList(id: '1', name: 'List #1', todos: []),
+          TodoList(id: '1', name: 'List #1', todos: [Todo(id: '1', name: 'Apples')]),
           TodoList(id: '2', name: 'List #2', todos: []),
           TodoList(id: '3', name: 'List #3', todos: []),
           TodoList(id: '4', name: 'List #4', todos: []),
